28d9ef843b5dcfc5921b8d004c13858e71c43fc3
Philippe Gerhardt <philippe@sutoiku.com>
2025-03-20 10:47:29 +0100

    Add modify and add actions for controller

--- Controllers/Api/CommentController.php
+++ Controllers/Api/CommentController.php
@@ -5,4 +5,33 @@ class CommentController extends BaseController
   {
     parent::__construct(new CommentModel());
   }
+
+  public function addAction(): void
+  {
+    $this->doAction($fn = function () {
+
+      $content = $this->getRequestBody('content');
+      $id_developer = $this->getRequestBody('id_developer');
+      $id_item = $this->getRequestBody('id_item');
+      return $this->model->add(array(
+        'content' => $content,
+        'id_developer' => $id_developer,
+        'id_item' => $id_item
+      ));
+    });
+  }
+
+  public function updateAction(): void
+  {
+    $this->doAction($fn = function () {
+      $id = $this->getQueryString('id');
+      $content = $this->getRequestBody('content');
+      return $this->model->modify(
+        array(
+          'id' => $id,
+          'content' => $content
+        )
+      );
+    });
+  }
 }
