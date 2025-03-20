4b9cf33af77b2558a6230d6609294a8ef9360a06
Philippe Gerhardt <philippe@sutoiku.com>
2025-03-20 10:48:08 +0100

    Update add and update actions for note

--- Controllers/Api/NoteController.php
+++ Controllers/Api/NoteController.php
@@ -9,9 +9,39 @@ class NoteController extends BaseController
   public function addAction(): void
   {
     $this->doAction($fn = function () {
-
       $title = $this->getRequestBody('title');
-      return $this->model->add(array('title' => $title, 'content' => '', 'type' => 'text'));
+      $content = $this->getRequestBody('content');
+      $id_users = $this->getRequestBody('id_users');
+      $is_public = $this->getRequestBody('is_public');
+      $id_programming_language = $this->getRequestBody('id_programming_language');
+      $id_project = $this->getRequestBody('id_project');
+      return $this->model->add(
+        array(
+          'title' => $title,
+          'content' => $content,
+          'type' => 'text',
+          'id_users' => $id_users,
+          'is_public' => $is_public,
+          'id_programming_language' => $id_programming_language,
+          'id_project' => $id_project
+        )
+      );
+    });
+  }
+
+  public function updateAction(): void
+  {
+    $this->doAction($fn = function () {
+      $id = $this->getRequestBody('id');
+      $title = $this->getRequestBody('title');
+      $content = $this->getRequestBody('content');
+      return $this->model->modify(
+        array(
+          'id' => $id,
+          'title' => $title,
+          'content' => $content
+        )
+      );
     });
   }
 }
