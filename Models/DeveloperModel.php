52083c4d19d59477a41901ec5e86227b413d6e83
Philippe Gerhardt <philippe@sutoiku.com>
2025-04-09 11:28:06 +0200

    Update developers

--- Models/DeveloperModel.php
+++ Models/DeveloperModel.php
@@ -12,19 +12,20 @@ class DeveloperModel extends Database implements IModel
 
     $this->baseQuery = <<<SQL
       SELECT
-          developer.id_users,
-          users.username,
-          users.email,
-          users.avatar_url,
-          users.created_at,
-          users.is_admin,
+          user.id_user,
+          user.username,
+          user.email,
+          user.avatar_url,
+          user.created_at,
+          user.is_admin,
+          user.id_role,
           role.name AS role
       FROM
-          users
+          user
       JOIN
-          developer ON users.id_users = developer.id_users
+          developer ON user.id_user = developer.id_user
       JOIN
-          role ON users.id_role = role.id_role
+          role ON user.id_role = role.id_role
 
       SQL;
   }
@@ -42,35 +43,61 @@ class DeveloperModel extends Database implements IModel
 
   public function getOne($id)
   {
-    $query = $this->baseQuery . " WHERE developer.id_users = ?";
+    $query = $this->baseQuery . " WHERE user.id_user = ?";
 
     return $this->selectOne($query, ["i", $id]);
   }
 
   public function remove($id)
   {
-    return $this->delete("DELETE FROM developer WHERE id_users = ?", ["i", $id]);
+    return $this->delete("DELETE FROM user WHERE id_user = ?", ["i", $id]);
   }
 
   public function add($paramsArray)
   {
-    $name = $paramsArray['name'];
+    $username = $paramsArray['username'];
     $email = $paramsArray['email'];
+    $avatar_url = $paramsArray['avatar_url'];
+    $role_id = $paramsArray['role_id'];
+    $is_admin = $paramsArray['is_admin'];
     $now = date('Y-m-d H:i:s');
-    return $this->insert(
-      "INSERT INTO developer (username, email, created_at) VALUES (?, ?, ?)",
-      ["sss", $name, $email, $now]
+    $id = $this->insert(
+      "INSERT INTO user (username, email, avatar_url, id_role, is_admin, created_at) " .
+      "VALUES (?, ?, ?, ?, ?, ?)",
+      ["sssiis", $username, $email, $avatar_url, $id_role, $is_admin, $now]
     );
+    $this->insert(
+      "INSERT INTO developer (id_user) VALUES (?)",
+      ["i", $id]
+    );
+
+    $query = $this->baseQuery . <<<SQL
+    WHERE user.id_user = ?
+    SQL;
+    return $this->selectOne($query, ["i", $id]);
   }
 
   public function modify($paramsArray)
   {
     $id = $paramsArray['id'];
-    $name = $paramsArray['name'];
-    $email = $paramsArray['email'];
-    return $this->update(
-      "UPDATE developer SET username = ?, email = ? WHERE id_users = ?",
-      ["ssi", $name, $email, $id]
+
+    $query = $this->baseQuery . <<<SQL
+    WHERE user.id_user = ?
+    SQL;
+    $user = $this->selectOne($query, ["i", $id]);
+
+    $username = $paramsArray['username'] ?? $user->username;
+    $email = $paramsArray['email'] ?? $user->email;
+    $avatar_url = $paramsArray['avatar_url'] ?? $user->avatar_url;
+    $id_role = $paramsArray['id_role'] ?? $user->id_role;
+    $is_admin = $paramsArray['is_admin'] ?? $user->is_admin;
+
+    $this->update(
+      "UPDATE user SET username = ?, email = ?, avatar_url = ?, id_role = ?, is_admin = ? " .
+      "WHERE user.id_user = ?",
+      ["sssiii", $username, $email, $avatar_url, $id_role, $is_admin, $id]
     );
+
+    return $this->selectOne($query, ["i", $id]);
   }
 }
