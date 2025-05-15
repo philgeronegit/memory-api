1fad664dc84b15c7855087b98c6f1f35c485e838
Philippe Gerhardt <philippe@sutoiku.com>
2025-05-15 11:23:52 +0200

    Update login model

--- Models/LoginModel.php
+++ Models/LoginModel.php
@@ -10,9 +10,20 @@ class LoginModel extends Database
     $password = $paramsArray['password'];
 
     $query = <<<SQL
-    SELECT id_user, username, password
-    FROM user
-    WHERE username = ? AND password = ?
+      SELECT
+          u.id_user,
+          u.username,
+          u.email,
+          u.avatar_url,
+          u.created_at,
+          u.id_role,
+          r.name as role_name,
+          r.role as role_value
+      FROM
+          user u
+      JOIN role r ON r.id_role = u.id_role
+      WHERE u.username = ? AND u.password = ?
+
     SQL;
 
     return $this->selectOne($query, ["ss", $username, $password]);
