53a1ed884ac460ccdfc8438955dc189a07cbe5a1
Philippe Gerhardt <philippe@sutoiku.com>
2025-11-26 12:38:07 +0100

    Add note access verification and required field checks in comment addition

--- Controllers/Api/CommentController.php
+++ Controllers/Api/CommentController.php
@@ -1,9 +1,12 @@
 <?php
 class CommentController extends BaseController
 {
+  protected $noteModel;
+
   public function __construct()
   {
     parent::__construct(new CommentModel());
+    $this->noteModel = new NoteModel();
   }
 
   public function addAction(): void
@@ -12,6 +15,20 @@ class CommentController extends BaseController
       $content = $this->getRequestBody('content');
       $id_user = $this->getRequestBody('id_user');
       $id_item = $this->getRequestBody('id_item');
+
+      // check if required fields are present
+      // return error whith http 400
+      if (empty($content) || empty($id_user) || empty($id_item)) {
+        $this->sendOutput('Missing required fields: content, id_user, or id_item.', array('HTTP/1.1 400 Bad Request'));
+        return;
+      }
+
+      // Verify user has access to the note before allowing comment
+      if (!$this->noteModel->userHasNoteAccess($id_item, $id_user)) {
+        $this->sendOutput('Unauthorized to update this user', array('HTTP/1.1 403 Forbidden'));
+        return;
+      }
+
       return $this->model->add(array(
         'content' => $content,
         'id_user' => $id_user,
