949e56e09ce736078130ac1b475eb2408ecd8d12
Philippe Gerhardt <philippe@sutoiku.com>
2026-01-13 16:08:54 +0100

    Add method documentation for Database class methods

--- Models/Database.php
+++ Models/Database.php
@@ -19,6 +19,12 @@ class Database
     }
   }
 
+  /**
+   * Execute a SELECT query and return the results as an associative array.
+   * @param string $query The SQL query to execute.
+   * @param array $params The parameters to bind to the query.
+   * @return array The result set as an associative array.
+   */
   public function select($query = "", $params = [])
   {
     try {
@@ -31,6 +37,12 @@ class Database
     }
   }
 
+  /**
+   * Execute a SELECT query and return a single result as an object.
+   * @param string $query The SQL query to execute.
+   * @param array $params The parameters to bind to the query.
+   * @return object The result as an object.
+   */
   public function selectOne($query = "", $params = []): object
   {
     try {
@@ -46,6 +58,12 @@ class Database
     }
   }
 
+  /**
+   * Execute an INSERT query and return the inserted ID.
+   * @param string $query The SQL query to execute.
+   * @param array $params The parameters to bind to the query.
+   * @return int The ID of the inserted row.
+   */
   public function insert($query = "", $params = [])
   {
     try {
@@ -57,6 +75,12 @@ class Database
     }
   }
 
+  /**
+   * Execute an UPDATE query.
+   * @param string $query The SQL query to execute.
+   * @param array $params The parameters to bind to the query.
+   * @return bool True on success.
+   */
   public function update($query = "", $params = [])
   {
     try {
@@ -68,6 +92,12 @@ class Database
     }
   }
 
+  /**
+   * Execute a DELETE query.
+   * @param string $query The SQL query to execute.
+   * @param array $params The parameters to bind to the query.
+   * @return bool True on success.
+   */
   public function delete($query = "", $params = [])
   {
     try {
@@ -79,6 +109,12 @@ class Database
     }
   }
 
+  /**
+   * Execute a prepared statement with the given query and parameters.
+   * @param string $query The SQL query to execute.
+   * @param array $params The parameters to bind to the query.
+   * @return mysqli_stmt The executed statement.
+   */
   private function executeStatement($query = "", $params = [])
   {
     try {
