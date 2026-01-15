ec7d68079671d0488c0f119f5bcc10583d524903
Philippe Gerhardt <philippe@sutoiku.com>
2026-01-15 10:11:07 +0100

    Refactor ProjectController to utilize ProjectService for project operations and improve data handling

--- Controllers/Api/ProjectController.php
+++ Controllers/Api/ProjectController.php
@@ -1,19 +1,25 @@
 <?php
 class ProjectController extends BaseController
 {
+  private $projectService;
+
   public function __construct()
   {
     parent::__construct(new ProjectModel());
+    $projectService = new ProjectService($this->model);
+    $this->projectService = $projectService;
   }
 
   public function addAction(): void
   {
     $this->doAction($fn = function () {
+      $data = [
+        'name' => $this->getRequestBody('name'),
+        'description' => $this->getRequestBody('description'),
+        'id_user' => $this->getRequestBody('id_user'),
+      ];
 
-      $name = $this->getRequestBody('name');
-      $description = $this->getRequestBody('description');
-      $id_user = $this->getRequestBody('id_user');
-      return $this->sendOutput($this->model->add(array('name' => $name, 'description' => $description, 'id_user' => $id_user)));
+      return $this->sendOutput($this->projectService->createProject($data));
     });
   }
 
@@ -21,16 +27,21 @@ class ProjectController extends BaseController
   {
     $this->doAction($fn = function () {
       $id = $this->getUriSegments()[3];
-      $name = $this->getRequestBody('name');
-      $description = $this->getRequestBody('description');
 
-      return $this->sendOutput($this->model->modify(
-        array(
-          'id' => $id,
-          'name' => $name,
-          'description' => $description
-        )
-      ));
+      $data = [
+        'name' => $this->getRequestBody('name'),
+        'description' => $this->getRequestBody('description'),
+      ];
+
+      return $this->sendOutput($this->projectService->updateProject($id, $data));
+    });
+  }
+
+  public function removeAction(): void
+  {
+    $this->doAction($fn = function () {
+      $id = $this->getUriSegments()[3];
+      return $this->projectService->deleteProject($id);
     });
   }
 
@@ -38,9 +49,14 @@ class ProjectController extends BaseController
   {
     $this->doAction($fn = function () {
       $user_id = $this->getUriSegments()[3];
-      $user_ids = array($user_id);
       $project_id = $this->getUriSegments()[5];
-      return $this->sendOutput($this->model->addToProject(array('user_ids' => $user_ids, 'project_id' => $project_id)));
+
+      $data = [
+        'user_ids' => [$user_id],
+        'project_id' => $project_id,
+      ];
+
+      return $this->sendOutput($this->projectService->addUsersToProject($data));
     });
   }
 
@@ -48,9 +64,14 @@ class ProjectController extends BaseController
   {
     $this->doAction($fn = function () {
       $user_id = $this->getUriSegments()[3];
-      $user_ids = array($user_id);
       $project_id = $this->getUriSegments()[5];
-      return $this->sendOutput($this->model->deleteFromProject(array('user_ids' => $user_ids, 'project_id' => $project_id)));
+
+      $data = [
+        'user_ids' => [$user_id],
+        'project_id' => $project_id,
+      ];
+
+      return $this->sendOutput($this->projectService->removeUsersFromProject($data));
     });
   }
 }
