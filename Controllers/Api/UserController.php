28e27ae7d9d9d45e5e04dfe466353878d117cff6
Philippe Gerhardt <philippe@sutoiku.com>
2026-01-15 10:11:14 +0100

    Refactor UserController to utilize UserService for user operations and enhance data handling

--- Controllers/Api/UserController.php
+++ Controllers/Api/UserController.php
@@ -1,74 +1,53 @@
 <?php
 class UserController extends BaseController
 {
+  private $userService;
+
   public function __construct()
   {
     parent::__construct(new UserModel());
+    $userService = new UserService($this->model);
+    $this->userService = $userService;
   }
 
   public function addAction(): void
   {
     $this->doAction($fn = function () {
-      if (!$this->hasUserModificationPermission()) {
-          $this->sendOutput('Unauthorized to add this user', array('HTTP/1.1 403 Forbidden'));
-          return;
-      }
+      $data = [
+        'username' => $this->getRequestBody('username'),
+        'email' => $this->getRequestBody('email'),
+        'avatar_url' => $this->getRequestBody('avatar_url'),
+        'id_role' => $this->getRequestBody('id_role'),
+        'is_admin' => $this->getRequestBody('is_admin'),
+        'password' => $this->getRequestBody('password'),
+      ];
 
-      $username = $this->getRequestBody('username');
-      $email = $this->getRequestBody('email');
-      $avatar_url = $this->getRequestBody('avatar_url');
-      $id_role = $this->getRequestBody('id_role');
-      $is_admin = $this->getRequestBody('is_admin');
-      $password = $this->getRequestBody('password');
-
-      $paramsArray = array(
-        'username' => $username,
-        'email' =>  $email,
-        'avatar_url' => $avatar_url,
-        'id_role' => $id_role,
-        'is_admin' => $is_admin,
-        'password' => $password
-      );
-      $validationErrors = $this->model->validate($paramsArray);
-      if ($validationErrors['hasErrors']) {
-          $this->sendOutput(['error' => $validationErrors['error']], $validationErrors['httpHeader']);
-          return;
-      }
-
-      return $this->sendOutput($this->model->add($paramsArray));
+      return $this->sendOutput($this->userService->createUser($data));
     });
   }
 
   public function updateAction(): void
   {
     $this->doAction($fn = function () {
-      if (!$this->hasUserModificationPermission()) {
-          $this->sendOutput(['error' => 'Unauthorized to update this user'], array('HTTP/1.1 403 Forbidden'));
-          return;
-      }
-
       $id = $this->getUriSegments()[3];
-      $username = $this->getRequestBody('username');
-      $email = $this->getRequestBody('email');
-      $avatar_url = $this->getRequestBody('avatar_url');
-      $id_role = $this->getRequestBody('id_role');
-      $is_admin = $this->getRequestBody('is_admin');
 
-      $paramsArray = array(
-        'id' => $id,
-        'username' => $username,
-        'email' =>  $email,
-        'avatar_url' => $avatar_url,
-        'id_role' => $id_role,
-        'is_admin' => $is_admin
-      );
-      $validationErrors = $this->model->validateUpdate($paramsArray);
-      if ($validationErrors['hasErrors']) {
-          $this->sendOutput(['error' => $validationErrors['error']], $validationErrors['httpHeader']);
-          return;
-      }
+      $data = [
+        'username' => $this->getRequestBody('username'),
+        'email' => $this->getRequestBody('email'),
+        'avatar_url' => $this->getRequestBody('avatar_url'),
+        'id_role' => $this->getRequestBody('id_role'),
+        'is_admin' => $this->getRequestBody('is_admin'),
+      ];
 
-      return $this->sendOutput($this->model->modify($paramsArray));
+      return $this->sendOutput($this->userService->updateUser($id, $data));
+    });
+  }
+
+  public function removeAction(): void
+  {
+    $this->doAction($fn = function () {
+      $id = $this->getUriSegments()[3];
+      return $this->userService->deleteUser($id);
     });
   }
 }
